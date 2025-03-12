bc911a41f971d89450619fe71efd5174456fd3f6
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2025-03-12 17:09:56 -0500

    Cambios en DockerFile

--- ProducerJava/demoProducer/src/main/java/producer/rabb/demo/ProducerController.java
+++ ProducerJava/demoProducer/src/main/java/producer/rabb/demo/ProducerController.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
